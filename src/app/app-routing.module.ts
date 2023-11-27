75c25eee846233feeb9580c0fdf6ae77cee2801e
Sythrex <ca.herreraa@duocuc.cl>
2023-11-27 13:28:08 -0300

    mike1.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,49 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
+import { ViajeComponent } from './viaje/viaje.component';
+import { ViajeconduComponent } from './viajecondu/viajecondu.component';
+import { AuthGuard } from './auth/auth.guard';
+import { ViajeModule } from './viaje/viaje.module';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'registro',
+    component: RegistroComponent,
+    
+    
+  },
+  {
+    path: 'viaje',
+    component: ViajeComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'viajecondu',
+    component: ViajeconduComponent,
+    canActivate: [AuthGuard],
+    
+  },
+
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+    
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+    
   },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
